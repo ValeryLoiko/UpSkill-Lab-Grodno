279d3d90b92b028ddffb477adec58748377fd49e
ValeryLoiko <77268554+ValeryLoiko@users.noreply.github.com>
2021-01-12 01:09:02 +0300

    Add files via upload

---
+++ JavaRush/Семейная перепись/Solution.java
@@ -0,0 +1,70 @@
+package com.javarush.task.task07.task0724;
+
+/* 
+Семейная перепись
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Human grandFather1 = new Human("Franc", true, 78);
+        Human grandFather2 = new Human("Valodia", true, 75);
+        Human grandMother1 = new Human("Al'bina", false, 95);
+        Human grandMother2 = new Human("Valia" , false, 71);
+        Human father = new Human("Roman", true, 61, grandFather1, grandMother2);
+        Human mother = new Human("Elena", false, 53, grandFather2, grandMother2);
+        Human child1 = new Human("Sasha", true, 31, father, mother);
+        Human child2 = new Human("Sergej", true, 28, father, mother);
+        Human child3 = new Human("Valera", true, 28, father, mother);
+
+        System.out.println(grandFather1);
+        System.out.println(grandFather2);
+        System.out.println(grandMother1);
+        System.out.println(grandMother2);
+        System.out.println(father);
+        System.out.println(mother);
+        System.out.println(child1);
+        System.out.println(child2);
+        System.out.println(child3);
+        // напишите тут ваш код
+    }
+
+    public static class Human {
+        String name;
+        boolean sex;
+        int age;
+        Human father;
+        Human mother;
+
+        public Human(String name, boolean sex, int age){
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+        }
+        public Human(String name, boolean sex, int age, Human father, Human mother){
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+            this.father = father;
+            this.mother = mother;
+        }
+
+        // напишите тут ваш код
+
+        public String toString() {
+            String text = "";
+            text += "Имя: " + this.name;
+            text += ", пол: " + (this.sex ? "мужской" : "женский");
+            text += ", возраст: " + this.age;
+
+            if (this.father != null) {
+                text += ", отец: " + this.father.name;
+            }
+
+            if (this.mother != null) {
+                text += ", мать: " + this.mother.name;
+            }
+
+            return text;
+        }
+    }
+}
