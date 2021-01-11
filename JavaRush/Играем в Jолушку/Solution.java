53b835edf4352e6e47e43ebfb090351232f4cff2
ValeryLoiko <77268554+ValeryLoiko@users.noreply.github.com>
2021-01-12 01:02:43 +0300

    Update Solution.java

--- JavaRush/Играем в Jолушку/Solution.java
+++ JavaRush/Играем в Jолушку/Solution.java
@@ -6,6 +6,12 @@ import java.util.ArrayList;
 
 /* 
 Играем в Jолушку
+1. Введи с клавиатуры 20 чисел, сохрани их в список и рассортируй по трём другим спискам:
+Число нацело делится на 3 (x%3==0), нацело делится на 2 (x%2==0) и все остальные.
+Числа, которые делятся на 3 и на 2 одновременно, например 6, попадают в оба списка.
+Порядок объявления списков очень важен.
+2. Метод printList должен выводить на экран все элементы списка с новой строки.
+3. Используя метод printList выведи эти три списка на экран. Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
 */
 
 public class Solution {
@@ -26,8 +32,6 @@ public class Solution {
              if (list.get(i) %2 == 0)
                 del2.add(list.get(i));
 
-//             if (list.get(i) %2 == 0 && list.get(i) %3 == 0)
-//                del2.add(list.get(i));
             else if (list.get(i) %3 != 0 && list.get(i) %2 != 0)
                 allLast.add(list.get(i));
 
@@ -36,14 +40,13 @@ public class Solution {
         printList(del3);
         printList(del2);
         printList(allLast);
-       // printList(list);
-        //напишите тут ваш код
+
     }
 
 
     public static void printList(ArrayList<Integer> list) {
       for (Integer x : list)
           System.out.println(x);
-        //напишите тут ваш код
+
     }
 }
