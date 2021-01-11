4d892a6ff709a9676734441b3c382f591fef2433
ValeryLoiko <77268554+ValeryLoiko@users.noreply.github.com>
2021-01-12 01:38:38 +0300

    Add files via upload

---
+++ JavaRush/Минимальное из N чисел/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task08.task0822;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/* 
+Минимальное из N чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        List<Integer> integerList = getIntegerList();
+        System.out.println(getMinimum(integerList));
+    }
+
+    public static int getMinimum(List<Integer> array) {
+
+       int b = Collections.min(array);
+        // Найти минимум тут
+        return b;
+    }
+
+    public static List<Integer> getIntegerList() throws IOException {
+       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+       int a = Integer.parseInt(reader.readLine());
+
+        List <Integer> spisok = new ArrayList<>();
+        for (int i = 0; i < a; i++){
+            int b = Integer.parseInt(reader.readLine());
+            spisok.add(i,b);
+        }
+
+        // Создать и заполнить список тут
+        return spisok;
+    }
+}
