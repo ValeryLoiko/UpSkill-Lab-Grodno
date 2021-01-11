22e8dc56d5a49e244f8633e318f3b95e9bc19799
ValeryLoiko <77268554+ValeryLoiko@users.noreply.github.com>
2021-01-12 00:18:07 +0300

    Add files via upload

---
+++ JavaRush/Количество дней в году/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task04.task0414;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Количество дней в году
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+      int a = Integer.parseInt(reader.readLine());
+      if (a%400==0) System.out.println("количество дней в году: 366");
+      else if (a%400!=0 && a%100==0) System.out.println("количество дней в году: 365");
+      else if (a%400!=0 && a%100!=0 && a%4==0) System.out.println("количество дней в году: 366");
+      else System.out.println("количество дней в году: 365");
+
+
+
+        //напишите тут ваш код
+
+    }
+}
