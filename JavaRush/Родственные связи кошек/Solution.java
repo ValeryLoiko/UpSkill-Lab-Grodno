33ec019ca30c0fd7fab03bf55187f61fd99330a8
ValeryLoiko <77268554+ValeryLoiko@users.noreply.github.com>
2021-01-12 00:55:03 +0300

    Update Solution.java

--- JavaRush/Родственные связи кошек/Solution.java
+++ JavaRush/Родственные связи кошек/Solution.java
@@ -6,6 +6,31 @@ import java.io.InputStreamReader;
 
 /* 
 Родственные связи кошек
+Задача: У каждой кошки есть имя и кошка-мама.
+Создать класс, который бы описывал данную ситуацию.
+Создать два объекта: кошку-дочь и кошку-маму.
+Вывести их на экран.
+
+Новая задача: У каждой кошки есть имя, кот-папа и кошка-мама.
+Изменить класс Cat так, чтобы он мог описать данную ситуацию.
+Создать 6 объектов: дедушку (папин папа), бабушку (мамина мама), папу, маму, сына, дочь.
+Вывести их всех на экран в порядке: дедушка, бабушка, папа, мама, сын, дочь.
+
+Пример ввода:
+дедушка Вася
+бабушка Мурка
+папа Котофей
+мама Василиса
+сын Мурчик
+дочь Пушинка
+
+Пример вывода:
+The cat's name is дедушка Вася, no mother, no father
+The cat's name is бабушка Мурка, no mother, no father
+The cat's name is папа Котофей, no mother, father is дедушка Вася
+The cat's name is мама Василиса, mother is бабушка Мурка, no father
+The cat's name is сын Мурчик, mother is мама Василиса, father is папа Котофей
+The cat's name is дочь Пушинка, mother is мама Василиса, father is папа Котофей
 */
 
 public class Solution {
@@ -47,10 +72,6 @@ public class Solution {
             this.name = name;
         }
 
-//        Cat(String name, Cat parent) {
-//            this.name = name;
-//            this.parent = parent;
-//        }
 
         Cat(String name, Cat parent, Cat grandParent){
             this.name = name;
