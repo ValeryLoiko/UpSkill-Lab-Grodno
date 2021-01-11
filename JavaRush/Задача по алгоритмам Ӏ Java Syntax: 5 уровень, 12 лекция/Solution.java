0f317d12f471dbdd983e1505dd2e62732cb3bc2f
ValeryLoiko <77268554+ValeryLoiko@users.noreply.github.com>
2021-01-12 00:49:31 +0300

    Add files via upload

---
+++ JavaRush/Задача по алгоритмам Ӏ Java Syntax: 5 уровень, 12 лекция/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task05.task0532;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Задача по алгоритмам Ӏ Java Syntax: 5 уровень, 12 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+
+        if (a>0){
+            int maximum = Integer.parseInt(reader.readLine());
+            for (int i=1; i<a; i++){
+                int b = Integer.parseInt(reader.readLine());
+                if (maximum>b)
+                    maximum=maximum;
+                            else    maximum=b;
+            }
+            System.out.println(maximum);
+
+        }
+
+
+        //напишите тут ваш код
+
+
+    }
+}
