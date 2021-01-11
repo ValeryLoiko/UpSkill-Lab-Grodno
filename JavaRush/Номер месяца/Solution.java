b8baee9ee7958060392ac819b9518bc74717e725
ValeryLoiko <77268554+ValeryLoiko@users.noreply.github.com>
2021-01-12 01:34:32 +0300

    Add files via upload

---
+++ JavaRush/Номер месяца/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task08.task0828;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 
+Номер месяца
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList <String> list = new ArrayList<>();
+        list.add("January");
+        list.add("February");
+        list.add("March");
+        list.add("April");
+        list.add("May");
+        list.add("June");
+        list.add("July");
+        list.add("August");
+        list.add("September");
+        list.add("October");
+        list.add("November");
+        list.add("December");
+
+        String month = reader.readLine();
+       if (list.contains(month))
+           System.out.println(month + " is the " + (list.indexOf(month) + 1) + " month");
+
+        //напишите тут ваш код
+    }
+}
