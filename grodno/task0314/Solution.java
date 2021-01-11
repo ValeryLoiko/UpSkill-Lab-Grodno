5a3a6d58eb4fbd23c35c48068c7dd491bca8826c
ValeryLoiko <77268554+ValeryLoiko@users.noreply.github.com>
2021-01-11 23:35:45 +0300

    Add files via upload

---
+++ grodno/task0314/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task03.task0314;
+
+/* 
+Таблица умножения Ӏ Java Syntax: 3 уровень, 6 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+        for (int a = 5; a < 11; a++) {
+
+
+            for (int b = 1; b < 11; b++)
+                System.out.print(a * b + " ");
+            System.out.println();
+        }
+       //напишите тут ваш код
+
+    }
+}
